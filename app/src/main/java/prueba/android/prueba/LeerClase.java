3106d3bf7b80a1be1e49b8af80041633fe4759cb
Mikel <francoquispe1995@gmail.com>
2018-09-03 11:20:22 -0500

    lectura de Tag

--- app/src/main/java/prueba/android/prueba/LeerClase.java
+++ app/src/main/java/prueba/android/prueba/LeerClase.java
@@ -1,55 +1,110 @@
 package prueba.android.prueba;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
-import android.view.Menu;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Parcelable;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+
 /**
- * Created by Franco on 10/01/2018.
+ * Created by Franco on 9/01/2018.
  */
 
-public class LeerClase extends Activity {
-
+public class LeerClase extends Activity
+{
+    NfcAdapter adapter;
+    PendingIntent pendingIntent;
+    IntentFilter readTagFilters[];
+    boolean readMode;
+    Tag tag;
+    Context context;
     TextView textView;
-    NfcAdapter nfcAdapter;
 
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leer);
+        context = this;
         textView = (TextView) findViewById(R.id.lectura);
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        adapter = NfcAdapter.getDefaultAdapter(this);
 
-    }
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-
-        enableForegroundDispatchSystem();
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        readTagFilters = new IntentFilter[]{tagDetected};
     }
 
     @Override
-    public void onPause()
-    {
-        super.onPause();
-        disableForegroundDispatchSystem();
-    }
+    protected void onNewIntent(Intent intent){
+        tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Toast.makeText(this,"leyendo tag", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public void onNewIntent(Intent intent)
-    {
-        super.onNewIntent();
-        if(intent.hasExtra(NfcAdapter.EXTRA_TAG))
-        {
-            Toast.makeText(this, "NFC Intent", Toast.LENGTH_SHORT).show();
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            tag.toString();
+        Parcelable[] parcelable = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (parcelable != null && parcelable.length > 0) {
+            Toast.makeText(this,"leyendo tag", Toast.LENGTH_SHORT).show();
+            readText((NdefMessage) parcelable[0]);
+
+        }else{
+            Toast.makeText(this,"Tag vacio", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void readText(NdefMessage ndefMessage){
+        NdefRecord[] ndefRecords = ndefMessage.getRecords();
+
+        if(ndefRecords != null && ndefRecords.length > 0){
+            NdefRecord ndefRecord = ndefRecords[0];
+            String tagContent = getText(ndefRecord);
+            textView.setText(tagContent);
+        }
+    }
+
+    public String getText(NdefRecord ndefRecord){
+        String tagContent = null;
+        try{
+            byte[] payload = ndefRecord.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize + 1,
+                    payload.length - languageSize - 1, textEncoding);
+        }catch (UnsupportedEncodingException e){
+            Log.e("getTextFromNdefRecord", e.getMessage(), e);
+        }
+        return tagContent;
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        WriteModeOff();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        WriteModeOn();
+    }
+
+    private void WriteModeOn(){
+        readMode = true;
+        adapter.enableForegroundDispatch(this, pendingIntent, readTagFilters, null);
+    }
+
+    private void WriteModeOff(){
+        readMode = false;
+        adapter.disableForegroundDispatch(this);
     }
 }
